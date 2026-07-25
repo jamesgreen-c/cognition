bd5e65e0fac5b9c32f6cb951e3a918af5cb38036
jamesgreen-c <jamesenricodavidgreen@gmail.com>
2026-07-25 16:25:25 +0100

    Add methods to get one-step filter distributions using RP GSSM

--- rp_ssm/recognition/rpm.py
+++ rp_ssm/recognition/rpm.py
@@ -5,8 +5,9 @@ import flax.linen as nn
 
 from jax import vmap, Array
 from rp_ssm.recognition.distmaps import DistMap
-from rp_ssm.distributions import NatParam
+from rp_ssm.distributions import NatParam, AllParam, GaussianDistParam
 from typing import Callable
+
 
 from rp_ssm.config import Config
 from rp_ssm.distributions import NatParam, LGStationaryParam, AllParams, NetworkParams
@@ -78,7 +79,44 @@ class RPSSM:
         outs = [vmap(vmap(lambda x: rec.apply(p, x)))(datapoint) for rec, p, datapoint in zip(self.recognition, rec_params, data)]
         # print(len(outs))
         return type(outs[0])(**{k: jnp.stack([out.params[k] for out in outs]) for k in outs[0].params.keys()})
-    
+
+    def initial_distribution(self, params, data):
+        """
+        Get filter distribution at T=0 given the initial prior and observation.
+
+        Parameters
+        ----------
+        params:  tuple (prior_params, *rec_params)
+        data:    (B, *D) T=0 observation for each recognition factor
+        """
+        prior_params, *rec_params = params
+        prior = self.prior.update(prior_params)
+
+        outs = [vmap(lambda x: rec.apply(p, x))(datapoint) for rec, p, datapoint in zip(self.recognition, rec_params, data)]
+        factors_nat = type(outs[0])(**{k: jnp.stack([out.params[k] for out in outs]) for k in outs[0].params.keys()}) # JxBxK
+        factors = AllParam(factors_nat.sum(axis=0)) # BxK
+
+        return initial_filter_distribution(prior, factors, self.latent_dim)
+
+    def filter(self, params, posterior, data):
+        """ 
+        Get filter distribution for next state given an observation 
+        
+        Parameters
+        ----------
+        params:  tuple (prior_params, *rec_params)
+        state:   (B, K) T=t-1 posterior to apply filter to
+        data:    (B, *D) T=t observation to get factor from
+        """
+        prior_params, *rec_params = params
+        prior = self.prior.update(prior_params)
+
+        outs = [vmap(lambda x: rec.apply(p, x))(datapoint) for rec, p, datapoint in zip(self.recognition, rec_params, data)]
+        factors_nat = type(outs[0])(**{k: jnp.stack([out.params[k] for out in outs]) for k in outs[0].params.keys()}) # JxBxK
+        factors = AllParam(factors_nat.sum(axis=0)) # BxK
+
+        return next_filter_distribution(prior, posterior, factors, self.latent_dim)
+
     def rollout(
             self, 
             params: dict, 
@@ -110,3 +148,82 @@ class RPSSM:
             latent_rollout.append(z_t)
         return jnp.stack(latent_rollout)  # shape: TxK
 
+
+def initial_filter_distribution(prior, factors, latent_dim):
+    """
+    Get the initial filter distribution given that:
+
+        p(z_0) = N(m1, Q1).
+        p(z_0 | x_0) = N(f.mean, f.cov)
+    """
+    # extract params and stats
+    m1 = prior.params["m1"]
+    Q1 = prior.params["Q1"]
+    I = jnp.eye(latent_dim)
+
+    factor_mean = factors.params["mean"]
+    factor_cov = factors.params["cov"]
+    factor_precision = jnp.linalg.solve(factor_cov, I)
+
+    # prior means and covs
+    batch_size = factor_mean.shape[0]
+    prior_mean = jnp.broadcast_to(m1, (batch_size, latent_dim))
+    prior_cov = jnp.broadcast_to(Q1, (batch_size, latent_dim, latent_dim))
+    prior_precision = jnp.linalg.solve(prior_cov, I)
+
+    # get posterior stats
+    posterior_precision = prior_precision + factor_precision
+    posterior_cov = jnp.linalg.solve(posterior_precision, I)
+
+    prior_precision_mean = jnp.einsum("bij,bj->bi", prior_precision, prior_mean)
+    factor_precision_mean = jnp.einsum("bij,bj->bi", factor_precision, factor_mean)
+    posterior_mean = jnp.einsum("bij,bj->bi", posterior_cov, prior_precision_mean + factor_precision_mean)
+
+    # enforce symmetry
+    posterior_cov = 0.5 * (posterior_cov + jnp.swapaxes(posterior_cov, -1, -2))
+
+    return GaussianDistParam(mean=posterior_mean, cov=posterior_cov)
+
+
+def next_filter_distribution(prior, posterior, factors, latent_dim):
+    """
+    Get the next filtering distribution given:
+        q(z_{t-1} | x_{0:t-1}) = N(m_{t-1}, P_{t-1})
+        p(z_t | x_{0:t-1}) = N(A m_{t-1} + b, A P_{t-1} A.T + Q)
+        f(z_t | x_t) = N(f.mean, f.cov)
+    """
+
+    # extract required params and stats
+    A = prior.params["A"]
+
+    if A.ndim == 1:
+            A = jnp.diag(A)
+
+    Q = prior.params["Q"]
+    b = prior.params["b"]
+    I = jnp.eye(latent_dim)
+
+    factor_mean = factors.params["mean"]
+    factor_cov = factors.params["cov"]
+    factor_precision = jnp.linalg.solve(factor_cov, I)
+
+    previous_mean = posterior.params["mean"]
+    previous_cov = posterior.params["cov"]
+
+    # Gaussian state dynamics
+    predicted_mean = previous_mean @ A.T + b
+    predicted_cov = jnp.einsum("ij,bjk,lk->bil", A, previous_cov, A) + Q
+    predicted_precision = jnp.linalg.solve(predicted_cov, I)
+
+    # get posterior stats
+    posterior_precision = predicted_precision + factor_precision
+    posterior_cov = jnp.linalg.solve(posterior_precision, I)
+
+    predicted_eta = jnp.einsum("bij,bj->bi", predicted_precision, predicted_mean)
+    factor_eta = jnp.einsum("bij,bj->bi", factor_precision, factor_mean)
+    posterior_mean = jnp.einsum("bij,bj->bi", posterior_cov, predicted_eta + factor_eta)
+
+    # enforce symmetry
+    posterior_cov = 0.5 * (posterior_cov + jnp.swapaxes(posterior_cov, -1, -2))
+
+    return GaussianDistParam(mean=posterior_mean, cov=posterior_cov)
